954ef4c96a6db2b393b331a1b8474c392b180c50
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-07-27 10:22:28 +0900

    add git

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'aws_blog'
 
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/norihide-yamanoi/aws_blog'
+set :repo_url, 'https://github.com/norihide-yamanoi/aws_blog.git'
 
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
 set :branch, ENV['BRANCH'] || 'master'
